01ade6819a93d04d39902bca9909a6dada85c21b
iibeeing <316553858@qq.com>
2016-06-29 15:44:24 +0800

    1、细化GenericDao的过程；

--- src/com/reflection/getGenericSuperclass/GenericDAO.java
+++ src/com/reflection/getGenericSuperclass/GenericDAO.java
@@ -4,18 +4,22 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
 public class GenericDAO<T> {
-	
+
 	private Class<T> entityClass;
 
 	protected GenericDAO() {
-		Type type = getClass().getGenericSuperclass();
+		Class clazz = getClass();
+		System.out.println(clazz);
+		Type type = clazz.getGenericSuperclass();
 		System.out.println(type);
-		Type trueType = ((ParameterizedType) type).getActualTypeArguments()[0];
+		Type [] types = ((ParameterizedType) type).getActualTypeArguments();
+		System.out.println(types);
+		Type trueType = types[0];
 		System.out.println(trueType);
 		this.entityClass = (Class<T>) trueType;
 	}
-	
-	public void outputEntityClass(){
-		System.out.println(entityClass);
+
+	public void outputEntityClass() {
+		System.out.println("此类的超类是：" + entityClass);
 	}
 }
